02edf8cfe6d0690a4ec236a79555f733ab0a2990
Anjireddy Avula <Anji.Reddy@fleetcor.com>
2025-10-15 11:59:24 +0530

    fix: corrected App.css import path

--- src/stores/usercart.jsx
+++ src/stores/usercart.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import '../../App.css';
+// import '../../App.css';
 import { UserCart } from "../stores/context/cart";
 import Navbar from "../stores/components/Navbar";
 
